80f207ecf8594c34384d92a598f1ccb14d9c14d6
unknown <yangl@szy.com>
2019-07-05 17:55:23 +0800

    init

---
+++ cryption.py
@@ -0,0 +1,20 @@
+"""字符串加解密模块"""
+
+
+class Cryption(object):
+
+    """
+    字符串加解密类
+    """
+
+    def __init__(self, action='decryption'):
+        
+
+    def decryption(self, tar_str):
+        pass
+
+    def encryption(self, tar_str):
+        pass
+
+    def __call__(self, tar_str):
+        pass
